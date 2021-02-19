936cf3363e2ba6ea338f67a23cf6010616d3b1e3
canvascat <canvascat@qq.com>
2021-02-19 13:17:37 +0800

    + type Clipboard ClipboardItem

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,9 +1,19 @@
 declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+  import { DefineComponent } from 'vue';
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
 }
 
-declare type Nullable<T> = T | null
+declare type Nullable<T> = T | null;
 
-declare type CustomizedHTMLElement<T> = HTMLElement & T
+declare type CustomizedHTMLElement<T> = HTMLElement & T;
+
+interface ClipboardItem {}
+declare var ClipboardItem: {
+  prototype: ClipboardItem;
+  new (data: { [type: string]: Blob }): ClipboardItem;
+};
+
+interface Clipboard {
+  write(dataTransfer: DataTransfer | Array<ClipboardItem>): Promise<void>;
+}
