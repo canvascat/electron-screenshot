a302bd3e7d7a5971a64cd12010655064808b9e21
canvascat <canvascat@qq.com>
2021-02-19 13:19:00 +0800

    ~ use store imageSource

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { blob } from './store'
+import { imageSource } from './store'
 
 import('./assets/bigbadfox1080p.mkv_20201108_104416.220.jpg')
   .then((res) => fetch(res.default))
   .then((r) => r.blob())
   .then((b) => {
-    const imageSource = new Image()
-    blob.value = imageSource.src = URL.createObjectURL(b)
-    imageSource.onload = () => createApp(App, { imageSource }).mount('#app')
+    imageSource.src = URL.createObjectURL(b)
+    imageSource.onload = () => createApp(App).mount('#app')
   })
