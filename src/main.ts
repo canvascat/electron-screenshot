1d4eb77ce802cc7f634517eb1d1174acf48bf0eb
canvascat <canvascat@qq.com>
2021-02-21 01:41:40 +0800

    ~ use loadImage

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { imageSource } from './store'
+import { loadImage } from './util/dom'
 
 import('./assets/bigbadfox1080p.mkv_20201108_104416.220.jpg')
   .then(res => fetch(res.default))
   .then(r => r.blob())
-  .then(b => {
-    imageSource.src = URL.createObjectURL(b)
-    imageSource.onload = () => createApp(App).mount('#app')
+  .then(async b => {
+    await loadImage(URL.createObjectURL(b), imageSource)
+    createApp(App).mount('#app')
   })
