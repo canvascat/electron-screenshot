7e38425d2d3c50ad60666f85126111a019627d66
canvascat <canvascat@qq.com>
2021-02-21 12:49:20 +0800

    + createNotification method

--- src/util/util.ts
+++ src/util/util.ts
@@ -20,3 +20,14 @@ export function rafThrottle<T extends AnyFunction<any>>(
 export function pointDistance([x0, y0]: Point, [x1, y1]: Point) {
   return ((x0 - x1) ** 2 + (y0 - y1) ** 2) ** 0.5
 }
+
+export async function createNotification(options?: NotificationOptions, title = document.title): Promise<Notification> {
+  if (!('Notification' in window)) throw new Error('This browser does not support notification')
+  if (Notification.permission === 'granted') {
+    options!.icon ||= document.querySelector('link[rel*=icon]')?.getAttribute('href') ?? undefined
+    return new Notification(title, options)
+  }
+  if (Notification.permission === 'denied') throw new Error('notification permission denied')
+  await Notification.requestPermission()
+  return await createNotification(options, title)
+}
