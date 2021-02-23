57c522417b9d831911af5e7a6a864c2eb9247546
canvascat <canvascat@qq.com>
2021-02-23 16:24:44 +0800

    + 百度统计

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import App from './App.vue'
 import { imageSource } from './store'
 import { loadImage } from './util/dom'
 
+if (import.meta.env.PROD) {
+  (<any>window)._hmt ??= []
+  const hm = document.createElement('script')
+  hm.src = 'https://hm.baidu.com/hm.js?4e02e53abd8605530e0c10481dabdca0'
+  const s = document.querySelector('script')
+  s!.parentNode!.insertBefore(hm, s)
+}
+
 import('./assets/bigbadfox1080p.mkv_20201108_104416.220.jpg')
   .then(res => fetch(res.default))
   .then(r => r.blob())
