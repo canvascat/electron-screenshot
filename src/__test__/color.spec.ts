b3103c25451e9e966df776af5dea5e92efa8f45c
canvascat <canvascat@qq.com>
2021-02-26 16:15:44 +0800

    + color test spec

---
+++ src/__test__/color.spec.ts
@@ -0,0 +1,8 @@
+import { default as Color } from 'src/util/color'
+
+describe('color', () => {
+  const color = new Color({ color: '#FF0000', format: 'rgb' })
+  it('color is', () => {
+    expect(color.color).toEqual('rgb(255, 0, 0)')
+  })
+})
