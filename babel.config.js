fc7362b1ae738ff808f5834f7554c172f6b8fbae
canvascat <canvascat@qq.com>
2021-02-26 11:16:37 +0800

    ~ update jest config

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,7 @@
 module.exports = {
-  // ATTENTION!!
-  // Preset ordering is reversed, so `@babel/typescript` will called first
-  // Do not put `@babel/typescript` before `@babel/env`, otherwise will cause a compile error
-  // See https://github.com/babel/babel/issues/12066
   presets: [
-    [
-      '@babel/env',
-      {
-        loose: true,
-        modules: false,
-      },
-    ],
-    '@babel/typescript',
+    // '@babel/typescript',
+    // '@babel/env',
+    '@vue/babel-preset-app',
   ],
 }
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,32 @@
 module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    '^.+\\js$': 'babel-jest',
-    '^.+\\.(t|j)sx?$': 'ts-jest',
+  globals: {
+    'ts-jest': {
+      babelConfig: true,
+    },
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  moduleFileExtensions: ['js','jsx','json','vue', 'ts', 'tsx'],
+  transform: {
+    // process *.vue files with vue-jest
+    '^.+\\.vue$': 'vue-jest',
+    // '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  // transformIgnorePatterns: ['/node_modules/'],
+  // support the same src -> src alias mapping in source code
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jsdom',
+  // serializer for snapshots
+  // snapshotSerializers: [
+  //   'jest-serializer-vue',
+  // ],
   // u can change this option to a more specific folder for test single component or util when dev
   // for example, ['<rootDir>/packages/input']
-  roots: ['<rootDir>'],
+  roots: ['<rootDir>/src'],
+  // testMatch: [
+  //   '**/tests/unit/**/*.spec.[jt]s?(x)',
+  //   '**/__tests__/*.[jt]s?(x)',
+  // ],
 }
