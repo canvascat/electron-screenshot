a13c50bee2b5a27500ebf9f03a2c2650c7a7ff1e
canvascat <canvascat@qq.com>
2022-09-28 23:37:38 +0800

    feat: 添加OffscreenCanvas类型定义

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+declare interface HTMLCanvasElement {
+  /** https://developer.mozilla.org/zh-CN/docs/Web/API/HTMLCanvasElement/transferControlToOffscreen */
+  transferControlToOffscreen: () => OffscreenCanvas;
+}
+
+/** https://developer.mozilla.org/zh-CN/docs/Web/API/OffscreenCanvas */
+declare interface OffscreenCanvas
+  extends MessagePort,
+  Pick<HTMLCanvasElement, 'height' | 'width' | 'getContext' | 'toBlob'> {
+  /** https://developer.mozilla.org/zh-CN/docs/Web/API/OffscreenCanvas/transferToImageBitmap */
+  transferToImageBitmap: () => ImageBitmap;
+}
