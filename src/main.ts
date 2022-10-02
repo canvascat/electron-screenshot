868c82abb49a71835b2ec4417e64602bfe0240fb
canvascat <canvascat@qq.com>
2022-10-02 23:14:53 +0800

    feat: 添加stats.js

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,14 @@ if (import.meta.env.PROD) {
   (<any>window)._hmt ??= [];
   const hm = document.createElement('script');
   hm.src = 'https://hm.baidu.com/hm.js?4e02e53abd8605530e0c10481dabdca0';
-  const s = document.querySelector('script');
-  s!.parentNode!.insertBefore(hm, s);
+  document.head.appendChild(hm);
+} else {
+  import('stats.js').then(({ default: Stats }) => {
+    const stats = new Stats();
+    const loop = () => (stats.update(), requestAnimationFrame(loop));
+    document.body.appendChild(stats.dom);
+    requestAnimationFrame(loop);
+  });
 }
 
 (async () => {
